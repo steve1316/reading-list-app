9f36ace18a94b1e97b04e554fa7fbe2cf4fb4f73
steve1316 <stevetu11@yahoo.com>
2020-04-20 20:40:09 -0700

    Provide theme context via Consumer instead of using contextType

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,24 +2,24 @@ import React, { Component } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
 
 class Navbar extends Component {
-    static contextType = ThemeContext;
     render() {
-        console.log(this.context);
-        // Decontructing
-        const { isLightTheme, light, dark } = this.context;
-
-        // If isLightTheme is true, set theme to light. Otherwise, set to dark.
-        const currentTheme = isLightTheme ? light : dark;
-
-        return (
-            <nav style={{ background: currentTheme.ui, color: currentTheme.syntax }}>
-                <h1>Context App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
+        return(
+            <ThemeContext.Consumer>{(context) => {
+                // Determine if theme is light or dark.
+                const { isLightTheme, light, dark } = this.context;
+                // If isLightTheme is true, set theme to light. Otherwise, set to dark.
+                const currentTheme = isLightTheme ? light : dark;
+                return (
+                    <nav style={{ background: currentTheme.ui, color: currentTheme.syntax }}>
+                        <h1>Context App</h1>
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Contact</li>
+                        </ul>
+                    </nav>
+                )
+            }}</ThemeContext.Consumer>
         );
     }
 }
