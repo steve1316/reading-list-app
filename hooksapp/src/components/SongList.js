5760f825d616d9184e09155856fd1f6b02b8849c
steve1316 <stevetu11@yahoo.com>
2020-04-22 09:46:42 -0700

    useEffect to log songs or age for every render

--- hooksapp/src/components/SongList.js
+++ hooksapp/src/components/SongList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import uuid from 'uuid/v1'
 import NewSongForm from './NewSongForm'
 
@@ -8,9 +8,18 @@ const SongList = () => {
     { title: 'Memory Gospel', id: 2 },
     { title: 'This Wild Darkness', id: 3 }
   ])
+  // On click, increment age by 1.
+  const [age, setAge] = useState(20)
   const addSong = title => {
     setSongs([...songs, { title: title, id: uuid() }])
   }
+  // useEffect's callback function will run every time the songs data changed, not the age data.
+  useEffect(() => {
+    console.log('useEffect hook ran', songs)
+  }, [songs])
+  useEffect(() => {
+    console.log('useEffect hook ran', age)
+  }, [age])
   return (
     <div className='song-list'>
       <ul>
@@ -19,6 +28,7 @@ const SongList = () => {
         })}
       </ul>
       <NewSongForm addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}>Add 1 to age: {age}</button>
     </div>
   )
 }
